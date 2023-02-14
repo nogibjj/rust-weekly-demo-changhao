1e86eda3d2755f8386e0164afe9cad0ae52e27f6
Changhao Li <71317967+june-rains@users.noreply.github.com>
2023-02-13 21:48:58 -0500

    Delete hello directory

--- hello/README.md
+++
@@ -1,9 +0,0 @@
-# ***Demo1 Hello***
-
-In this project, we have built a command line tool to play Marco Polo game by using rust combing with copilot.  
-
-To run this project, please follow below command line:  
-* `make format` 
-* `make lint`
-* `cargo run -- --help` 
-
--- hello/src/lib.rs
+++
@@ -1,9 +0,0 @@
-/*build a function marco polo which take a string as input, if input is Marco, return Polo, otherwise, return Who?*/
-
-pub fn marco_polo(input: &str) -> String {
-    if input == "Marco" {
-        "Polo".to_string()
-    } else {
-        "Who?".to_string()
-    }
-}
